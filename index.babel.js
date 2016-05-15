8c16ec08f9b71e48cabd1a36bd741955e24eb0bd
pnlybubbles <pnlybubbles@gmail.com>
2016-05-16 00:30:45 +0900

    feat: add download button

--- index.babel.js
+++ index.babel.js
@@ -8,17 +8,23 @@ import {vec3} from 'gl-matrix';
 const renderer = new Renderer('c');
 const rayTracer = new RayTrace(renderer.x, renderer.y);
 
-const redMaterial = new Material(vec3.fromValues(1, 0.2, 0.2), 0.5, 0, 0.5, 0);
-rayTracer.add(new CSphere(vec3.fromValues(0, 0, 0), 1, redMaterial));
-// rayTracer.add(new CTriangle(vec3.fromValues(1, 1, 0), vec3.fromValues(-1, 0.5, 0), vec3.fromValues(0, 0, 0)));
+const redMaterial = new Material(vec3.fromValues(0.6, 0.2, 0.2), 0.5, 0.5, 0, 0);
+const emissiveMaterial = new Material(vec3.fromValues(1, 0.9, 0.5), 0, 0, 0, 1);
+rayTracer.add(new CSphere(vec3.fromValues(0, 2, 0), 1, emissiveMaterial));
+rayTracer.add(new CTriangle(vec3.fromValues(1, -1, -0.5), vec3.fromValues(-1, -1, -0.5), vec3.fromValues(0, -2, 0.5), redMaterial));
 rayTracer.run();
 const data = rayTracer.getData();
 
 for (let x = 0; x < rayTracer.x; x++) {
   for (let y = 0; y < rayTracer.y; y++) {
     const color = data[y][x];
-    renderer.setPixel(x, y, color[0], color[1], color[2], color[3]);
+    renderer.setPixel(x, y, color[0], color[1], color[2], 1);
   }
 }
 
 renderer.render();
+
+// Download Button
+
+const canvas = document.getElementById('c');
+document.getElementById('download-button').href = canvas.toDataURL();
