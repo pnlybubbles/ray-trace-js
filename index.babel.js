f7e2005f120d6f30faba2055df406d8848b974be
pnlybubbles <pnlybubbles@gmail.com>
2016-05-16 14:23:20 +0900

    feat: add room and light

--- index.babel.js
+++ index.babel.js
@@ -8,20 +8,108 @@ import {vec3} from 'gl-matrix';
 const renderer = new Renderer('c');
 const rayTracer = new RayTrace(renderer.x, renderer.y);
 
-const redMaterial = new Material(vec3.fromValues(0.6, 0.2, 0.2), 0.5, 0.5, 0, 0);
-const emissiveMaterial = new Material(vec3.fromValues(1, 0.9, 0.5), 0, 0, 0, 1);
-rayTracer.add(new CSphere(vec3.fromValues(0, 2, 0), 1, emissiveMaterial));
-rayTracer.add(new CTriangle(vec3.fromValues(1, -1, -0.5), vec3.fromValues(-1, -1, -0.5), vec3.fromValues(0, -2, 0.5), redMaterial));
-rayTracer.run();
-const data = rayTracer.getData();
+const redMaterial = new Material(vec3.fromValues(1, 0, 0), 1, 0, 0, 0);
+const blueMaterial = new Material(vec3.fromValues(0, 0, 1), 1, 0, 0, 0);
+const whiteMaterial = new Material(vec3.fromValues(1, 1, 1), 1, 0, 0, 0);
+const emmisiveMaterial = new Material(vec3.fromValues(1, 0.95, 0.7), 0, 0, 0, 1);
+// rayTracer.add(new CSphere(vec3.fromValues(0, 2, 0), 1, emmisiveMaterial));
+const objects = [
+  {
+    type: CTriangle,
+    args: [[-5, 5, 0], [-5, -5, 0], [-5, 5, -10], redMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[-5, -5, 0], [-5, -5, -10], [-5, 5, -10], redMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[5, -5, 0], [5, 5, 0], [5, 5, -10], blueMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[5, -5, -10], [5, -5, 0], [5, 5, -10], blueMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[-5, 5, -10], [-5, -5, -10], [5, 5, -10], whiteMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[-5, -5, -10], [5, -5, -10], [5, 5, -10], whiteMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[-5, -5, -10], [-5, -5, 0], [5, -5, -10], whiteMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[-5, -5, 0], [5, -5, 0], [5, -5, -10], whiteMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[-5, 5, 0], [-5, 5, -10], [5, 5, -10], whiteMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[5, 5, 0], [-5, 5, 0], [5, 5, -10], whiteMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[-1, 4.99, -4], [-1, 4.99, -5], [1, 4.99, -5], emmisiveMaterial],
+  },
+  {
+    type: CTriangle,
+    args: [[1, 4.99, -4], [-1, 4.99, -4], [1, 4.99, -5], emmisiveMaterial],
+  },
+];
+objects.forEach((o) => {
+  const args = [];
+  o.args.forEach((a) => {
+    if (Array.isArray(a)) {
+      args.push(vec3.fromValues(a[0], a[1], a[2]));
+    } else {
+      args.push(a);
+    }
+  });
+  rayTracer.add(new o.type(...args));
+});
 
+const count = 50;
+let countRendered = 0;
+
+// render and show one by one
+// function render() {
+//   if (count <= countRendered) {
+//     return;
+//   }
+//   console.log(count);
+//   rayTracer.run();
+//   const data = rayTracer.getData();
+//   for (let x = 0; x < rayTracer.x; x++) {
+//     for (let y = 0; y < rayTracer.y; y++) {
+//       const color = data[y][x];
+//       renderer.setPixel(x, y, color[0], color[1], color[2], 1);
+//     }
+//   }
+//   renderer.render();
+//   countRendered += 1;
+//   requestAnimationFrame(render);
+// }
+// render();
+
+// render all and show one
+for (let i = 0; i < count; i++) {
+  console.log(i);
+  rayTracer.run();
+}
+const data = rayTracer.getData();
 for (let x = 0; x < rayTracer.x; x++) {
   for (let y = 0; y < rayTracer.y; y++) {
     const color = data[y][x];
     renderer.setPixel(x, y, color[0], color[1], color[2], 1);
   }
 }
-
 renderer.render();
 
 // Download Button
